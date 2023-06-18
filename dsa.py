881e972d735bb2d5767615d8f2314f3228e8c744
Christine Mosqueda <mosqueda.christinereisa04@gmail.com>
2023-06-18 12:51:33 +0800

    first commit

---
+++ dsa.py
@@ -0,0 +1,15 @@
+# magbuhat ug python program nga naga store tanan topics sa DSA final term.
+# dapat mag use ug import2 ug files
+
+
+class DSA_Topics:
+    def __init__(self):
+        print('Hello, world!')
+
+
+
+def main():
+    dsa_program = DSA_Topics()
+
+if __name__ == '__main__':
+    main()
